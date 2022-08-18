e96d5928dbfb0a32b7d5aba10d3973afa3de8053
Rob De Feo <robdefeo@gmail.com>
2022-08-18 10:00:41 +0200

    chore: prepare packages for publishing
    * refactor: build->dist
    
    * chore: have nx create encoding distribution
    
    * chore: add changeset
    
    * chore: move signatures out of crypto library
    
    * chore: clean up tests
    
    * chore: create crypto build
    
    * chore: remove encoding from crypto package
    
    * chore: fix tests
    
    * chore: remove arrayify from crypto
    
    * chore: create protocols package
    
    * refactor: internal -> addressing
    
    * refactor: protocols into addressing
    
    * refactor: move keyreg to sdk
    
    * chore: prep build
    
    * docs: small updates
    
    * docs: `mailchain.self()`
    
    * refactor: don't use deep imports
    
    * chore: ensure all dependencies are correct
    
    * chore: fix formatting
    
    * chore: rename readme step one
    
    * chore: rename Readme
    
    * chore: first publish command
    
    * chore: try to fix build
    
    * chore: add missing build dependencies
    
    * test: fix failing test
    
    * refactor: better name for response
    
    * chore: update docs
    
    * chore: remove unneeded resource
    
    * chore: fix user test
    
    * chore: rework imports
    
    * chore: protobufjs is required
    
    * chore: rename self->user
    
    GitOrigin-RevId: 6705c7ebb6b0cdfd1573bb171cf746139d846462

--- signatures/consts.go
+++
@@ -1,8 +0,0 @@
-package signatures
-
-const (
-	KindEthereumPersonalMessage = "ethereum_personal_message"
-	// KindRawED25519 signs the message with the curve's native sign method with no pre-processing.
-	KindRawED25519                   = "raw_ed25519"
-	KindMailchainUsernameIdentityKey = "mailchain_username_identity_key"
-)
--- signatures/errors.go
+++
@@ -1,5 +0,0 @@
-package signatures
-
-import "errors"
-
-var ErrKeyNotSupported = errors.New("key not supported")
